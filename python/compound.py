d1a3556f803bd2bfd5533fd8bc52f34ee9f43ae1
suganthicj <52270901+suganthicj@users.noreply.github.com>
2020-01-07 23:32:06 -0800

    Create compound.py

---
+++ python/compound.py
@@ -0,0 +1,8 @@
+p = float(input("Enter the principle amount : "))
+r = float(input("Enter the rate of interest : "))
+t = float(input("Enter the time in the years: "))
+ci =  p * (pow((1 + r / 100), t)) 
+print("Principle amount  : ", p)
+print("Interest rate     : ", r)
+print("Time in years     : ", t)
+print("compound Interest : ", ci)
